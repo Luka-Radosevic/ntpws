307b0b2c8638e4f7018da1debb7b94da86e24fda
Luka-Radosevic <80633011+Luka-Radosevic@users.noreply.github.com>
2024-11-11 20:48:48 +0100

    Add files via upload

---
+++ vjezba2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+    <html lang="hr">
+        <head>
+            <title>Programiranje web aplikacija</title>
+        </head>
+    <body>
+        <h1>Naslov</h1>
+        <h2>Podnaslov</h2>
+        <p>Odlomak 1</p>
+        <p>Odlomak 2</p>
+        <img src = "images/phpIcon.png" width = "300" height = "200" alt = "relative image" title = "relative image">
+        <br>
+        <img src="https://pngimg.com/uploads/php/php_PNG18.png" width="300" height = "200" alt = "absolute image" title = "absolute image"/>
+    </body>
+    </html>
