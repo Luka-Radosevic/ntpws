ba5c79f2e8e20b47bc1f76c40c9bb56ace938990
Luka-Radosevic <80633011+Luka-Radosevic@users.noreply.github.com>
2024-11-11 20:32:47 +0100

    Add files via upload

---
+++ vjezba1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+    <html lang="hr">
+        <head>
+            <title>Programiranje web aplikacija</title>
+        </head>
+    <body>
+        <h1>Naslov</h1>
+        <h2>Podnaslov</h2>
+        <p>Odlomak 1</p>
+        <p>Odlomak 2</p>
+    </body>
+    </html>
