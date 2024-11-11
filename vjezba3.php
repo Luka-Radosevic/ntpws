ec487f3ab5049a73252ac800f2b8f6d9bef905b0
Luka-Radosevic <80633011+Luka-Radosevic@users.noreply.github.com>
2024-11-11 20:59:21 +0100

    Add files via upload

---
+++ vjezba3.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+    <html lang="hr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Napredne tehnike programiranja web servisa</title>
+        </head>
+    <body>
+        <h1>Naslov</h1>
+        <h2>Podnaslov</h2>
+        <p>Odlomak 1</p>
+        <p>Odlomak 2</p>
+        <figure>
+            <a href = "images/phpIcon.png" target = "_blank">
+                <img src = "images/phpIcon.png" width = "300" height = "200" alt = "Relativna slika" title = "Relativna slika">
+            </a>
+            <figcaption>Relativna slika - ikona PHP-a</figcaption>
+        </figure>
+        
+        <br>
+
+        <figure>
+            <a href = "images/phpIcon.png" target = "_blank">
+                <img src="https://pngimg.com/uploads/php/php_PNG18.png" width="300" height = "200" alt = "Absolutna slika" title = "Absolutna slika"/>
+            </a>
+            <figcaption>Absolutna slika - ikona PHP-a</figcaption>
+        </figure>
+        
+    </body>
+    </html>
