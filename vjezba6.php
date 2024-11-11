d8d38e73f570983c8e0860f54fb820c936ff2cec
Luka-Radosevic <80633011+Luka-Radosevic@users.noreply.github.com>
2024-11-11 21:44:23 +0100

    Add files via upload

---
+++ vjezba6.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+    <html lang="hr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Napredne tehnike programiranja web servisa</title>
+        </head>
+        <body>
+        <h1>Neuređena lista</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>Apple</li>
+            <li>Huawei</li>
+            <li>Panasonic</li>
+            <li>Motorola</li>
+        </ul>
+        
+        <h1>Uređena lista</h1>
+        <ol>
+            <li>Samsung</li>
+            <li>Apple</li>
+            <li>Huawei</li>
+            <li>Panasonic</li>
+            <li>Motorola</li>
+        </ol>
+
+        <h1>Definicijska lista</h1>
+        <dl>
+            <dt>Apple</dt>
+            <dd>Iphone 6s</dd>
+            <dd>Iphone 6s Plus</dd>
+            <dd>Iphone 7s</dd>
+            <dd>Iphone 7s Plus</dd>
+            <dd>Ipad mini 4</dd>
+        </dl>
+
+        <h1>Ugnježdavanje lista</h1>
+        <ul>
+            <li>Samsung</li>
+            <ul>
+                <li>Galaxy A3</li>
+                <li>Galaxy S4</li>
+                <li>Galaxy S5</li>
+                <li>Galaxy S6</li>
+                <li>Galaxy S6 Edge</li>
+                <li>Galaxy S7</li>
+                <li>Galaxy S7 Edge</li>
+            </ul>
+        </ul>
+        </body>
+    </html>
