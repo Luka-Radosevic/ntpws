2ae020bdddd76bdc62babf973aa244bd7cbad943
Luka-Radosevic <80633011+Luka-Radosevic@users.noreply.github.com>
2024-11-11 21:37:07 +0100

    Add files via upload

---
+++ vjezba5.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML>
+    <html lang="hr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Napredne tehnike programiranja web servisa</title>
+        </head>
+        <body>
+            <h1>Popis mobitela</h1>
+            <table border = "1">
+                <tr>
+                    <th colspan = "2">Samsung</th>
+                    <th colspan = "2">Apple</th>
+
+                </tr>   
+                <tr>
+                    <td><center><img src = "https://drop.ndtv.com/TECH/product_database/images/480_2_4010_480X960_4272017121820PM.jpg?downsize=*:420&output-quality=80" width = "50px"></center></td>
+                    <td>Galaxy S8</td>
+                    <td><img src = "https://drop.ndtv.com/TECH/product_database/images/913201715733AM_635_iphone_8.jpeg?downsize=*:420&output-quality=80" width = "100px"></td>
+                    <td>Iphone 9</td>
+                </tr>
+                <tr>
+                    <td><img src = "https://i.gadgets360cdn.com/products/large/1519585112_635_samsung_galaxy_s9_plus_grey.jpg?downsize=*:420&output-quality=80" width = "100px"></td>
+                    <td>Galaxy S9</td>
+                    <td><img src = "https://drop.ndtv.com/TECH/product_database/images/913201720152AM_635_iphone_x.jpeg?downsize=*:420&output-quality=80" width = "100px"></td>
+                    <td>Iphone X</td>
+                </tr>
+            </table>  
+        </body>
+    </html>
