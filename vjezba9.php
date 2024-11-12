414afb3d4feff1bc0bc76cb79f83d406d056119c
Luka-Radosevic <80633011+Luka-Radosevic@users.noreply.github.com>
2024-11-12 21:56:34 +0100

    Add files via upload

---
+++ vjezba9.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+    <html lang="hr">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Planine</title>
+        </head>
+        <body>
+            <div>
+                <p>Lorem ipsum dolor <strong style = "color:red"> sit </strong>amet, consectetur adipiscing elit.</p>
+                <p>Sed volutpat urna <strong style = "color:red"> vitae </strong> lorem facilisis, sed vehicula magna rutrum.</p>
+            </div>
+
+            <p>Maecenas<strong style = "color:red"> vitae </strong>nibh massa.</p>
+            <p>Ut finibus non<strong style = "color:red"> sit </strong>nec ultrices.</p>
+        </body>
+    </html>
